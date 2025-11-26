7b03fb34d8808a12247f7f49799db04a47f07586
ahmedfyaz <awanahmed755@gmail.com>
2025-11-26 14:08:13 +0500

    Added home page

--- lib/ui/get_started.dart
+++ lib/ui/get_started.dart
@@ -1,14 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:untitled1/ui/welcome.dart';
+
+import '../models/constants.dart';
 class GetStarted extends StatelessWidget {
   const GetStarted({super.key});
 
   @override
   Widget build(BuildContext context) {
+    Constants constants = Constants();
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.lightBlueAccent,
-        title: Center(child: Text("Weather App"),),
-      ),
+      body: Container(
+        width: size.width,
+        height: size.height,
+        decoration: BoxDecoration(
+          color: constants.primaryColor.withOpacity(0.5),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset("assets/get-started.png"),
+            const SizedBox(
+              height: 30,
+            ),
+            GestureDetector(
+              onTap: (){
+                Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>Welcome()));
+              },
+              child: Container(
+                height: 50,
+                width: size.width * .7,
+                decoration: BoxDecoration(
+                  color: constants.secondaryColor,
+                  borderRadius: BorderRadius.circular(10)
+                ),
+                child: Center(
+                  child: Text("Get Started",style: TextStyle(color: Colors.white,fontSize: 18),),
+                ),
+              ),
+            )
+          ],
+        ),
+      )
     );
   }
 }
